6aec2abd6a8bf8594f596f1ec843f8f65c092eea
crockstylie <antoine.hory@gmail.com>
2021-04-26 11:27:02 +0200

    début du code from scratch (too long)

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     * @package ThemeGraciet
+     */
+?>
+            </div><!-- #content -->
+            <footer id="" class="" role="contentinfo">
+                <div class="">
+                    <div class="">
+
+                    </div><!-- .site-info -->
+                </div><!--  .container -->
+            </footer><!-- #colophon -->
+        </div><!-- #page -->
+        <?php wp_footer(); ?>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <!-- Bootstrap js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+    /**
+     * @package ThemeGraciet
+     */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Antoine HORY">
+        <title><?php echo get_bloginfo('name'); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <div id="page" class="container-fluid">
+            <header id="" class="row" role="banner">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header><!-- #masthead -->
+            <div id="" class="">
